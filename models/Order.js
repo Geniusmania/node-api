086bb18639af03d921c4e082c7fd5fa182a15f55
Geniusmania <baidoosolomon2001@gmail.com>
2025-04-10 09:55:29 +0000

    added protected middleware

--- models/Order.js
+++ models/Order.js
@@ -1,61 +1,52 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const orderItemSchema = new mongoose.Schema(
-	{
-		product: {
-			type: mongoose.Schema.Types.ObjectId,
-			ref: "Product",
-			required: true,
-		},
-		quantity: {
-			type: Number,
-			required: true,
-			min: 1,
-		},
-		price: {
-			type: Number,
-			required: true,
-		},
-	},
-	{ _id: false }
-);
-const orderSchema = new mongoose.Schema(
-	{
-		address: {
-			type: String,
-			required: true,
-		},
-		deliveryDate: {
-			type: Date, // or Date
-			required: true,
-		},
-		items: {
-			type: [orderItemSchema],
-			required: true,
-		},
-		orderDate: {
-			type: Date, // or Date
-			required: true,
-		},
-		paymentMethod: {
-			type: String,
-			required: true,
-		},
-		status: {
-			type: String,
-			required: true,
-		},
-		totalAmount: {
-			type: Number,
-			required: true,
-		},
-		userId: {
-			type: mongoose.Schema.Types.ObjectId,
-			ref: "User",
-			required: true,
-		},
-	},
-	{ timestamps: true }
-);
+const orderSchema = new mongoose.Schema({
+  address: {
+    name: { type: String, required: true },
+    city: { type: String, required: true },
+    state: { type: String, required: true },
+    street: { type: String, required: true },
+    postalCode: { type: String, required: true },
+    phoneNumber: { type: String, required: true },
+    country: { type: String, required: true },
+  },
+  deliveryDate: {
+    type: Date,
+    default: null,
+  },
+  items: [
+    {
+      productId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+      },
+      name: String,
+      quantity: Number,
+      price: Number,
+      image: String,
+    }
+  ],
+  orderDate: {
+    type: Date,
+    default: Date.now,
+  },
+  paymentMethod: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ['processing', 'shipped', 'delivered'],
+    default: 'processing',
+  },
+  totalAmount: {
+    type: Number,
+    required: true,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  }
+});
 
-module.exports = mongoose.model("Order", orderSchema);
+module.exports = mongoose.model('Order', orderSchema);
--- routes/middleware/Auth.js
+++ routes/middleware/Auth.js
@@ -29,14 +29,14 @@ const protect = async (req, res, next) => {
 };
 
 
-const admin = (req, res, next) => {
-  if (req.user && req.user.isAdmin) {
-    next();
-  } else {
-    res.status(403).json({ message: 'Not authorized as admin' });
-  }
-};
+// const admin = (req, res, next) => {
+//   if (req.user && req.user.isAdmin) {
+//     next();
+//   } else {
+//     res.status(403).json({ message: 'Not authorized as admin' });
+//   }
+// };
 
 
 module.exports =protect;
-module.exports = admin;
+// module.exports = admin;
